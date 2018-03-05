7c7038012d2508aad0883468df345fa2a807f46c
eelis.takala <eelis.takala@trafotek.fi>
2018-03-05 10:58:46 +0200

    Add some bashrc

---
+++ bash/git_aliases.sh
@@ -0,0 +1,11 @@
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# git aliases
+alias gs='git status'
+alias gco='git checkout'
+alias gc='git commit -m '
+alias gb='git branch'
+
