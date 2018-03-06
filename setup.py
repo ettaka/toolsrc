173f4e2f82c1408485dc4fbac5e2956a057da665
Eelis Takala <eelis.takala@gmail.com>
2018-03-06 09:27:41 +0200

    Add vim-plug install to nvim

--- setup.py
+++ setup.py
@@ -3,6 +3,14 @@
 import subprocess
 import os
 
+home_dir = os.path.expanduser("~")  
+working_dir = os.getcwd()
+nvim_install_dir = home_dir + "/bin"
+nvim_conf_path = home_dir + "/.config/nvim"
+nvim_init_path = nvim_conf_path + "/init.vim"
+nvim_init_target = working_dir + "/nvim/init.vim"
+vimplug_url = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 def e(cmd):
 	proc = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
 	return proc.stdout.read()
@@ -45,24 +53,36 @@ def install_nvim_appimage(install_path):
 		e('mv nvim.appimage ' + install_path) 
 		e('chmod u+x ' + install_path + '/nvim.appimage')
 		e('ln -s ' + install_path + '/nvim.appimage ' + install_path + '/nvim')
-
 	
 	else:
 		print "already exists"
+
+
+def install_vim_plug():
+    autoload_dir = nvim_conf_path + '/autoload'
+    vimplug_file_path = autoload_dir + '/plug.vim'
+    print "Fetcing plug.vim at", vimplug_url, "..."
+    if not os.path.isfile(vimplug_file_path):
+        try:
+            e('curl -LO ' + vimplug_url)
+        except:
+            print "Problems with fetching, check if curl works accordingly."
+            exit()
+        makedir(autoload_dir)
+        e('mv plug.vim ' + autoload_dir)
+    else:
+        print "exists already"
+                
+    if not os.path.isfile(vimplug_file_path):
+        print "A problem with fetching the vim.plug file"
+        exit()
 
 if git_version() == None:
 	print "Git is not found! Exiting..."
 	exit()
 
-home_dir = os.path.expanduser("~")  
-working_dir = os.getcwd()
-nvim_install_dir = home_dir + "/bin"
-nvim_conf_path = home_dir + "/.config/nvim"
-nvim_init_path = nvim_conf_path + "/init.vim"
-nvim_init_target = working_dir + "/nvim/init.vim"
-
 create_nvim_conf_path()
 create_nvim_init_symlink()
 install_nvim_appimage(nvim_install_dir)
-
+install_vim_plug()
 
