cd680cbf4d5783f28d06602a6b1351cb5d60a899
Eelis Takala <eelis@meetiqm.com>
2023-02-13 12:15:33 +0200

    Settings

--- nvim-lua/lua/ettaka/mappings.lua
+++ nvim-lua/lua/ettaka/mappings.lua
@@ -1,10 +1,10 @@
 vim.keymap.set("n", "<leader>t", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>ev", function () vim.cmd("vsp ~/.config/nvim/init.lua") end)
+vim.keymap.set("n", "<leader>ev", function () vim.cmd("vsp ~/.config/nvim/") end)
 vim.keymap.set("n", "<C-h>", "<C-w>h:res<CR>")
 vim.keymap.set("n", "<C-j>", "<C-w>j:res<CR>")
 vim.keymap.set("n", "<C-k>", "<C-w>k:res<CR>")
 vim.keymap.set("n", "<C-l>", "<C-w>l:res<CR>")
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+vim.keymap.set("t", "<leader><Esc>", "<C-\\><C-n>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
