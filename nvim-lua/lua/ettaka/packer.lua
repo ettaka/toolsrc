2a6ac91aa49696fdf83861e572f00da5b6622607
Eelis Takala <eelis@meetiqm.com>
2023-07-13 10:37:16 +0300

    Add lualine

--- nvim-lua/lua/ettaka/packer.lua
+++ nvim-lua/lua/ettaka/packer.lua
@@ -45,4 +45,8 @@ return require('packer').startup(function(use)
     }
 
     use 'ettaka/vim-elmer'
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 end)
