e59cbe29f3cf517ca9f1ba5194fc5c67ff5f7b88
Eelis Takala <eelis@meetiqm.com>
2023-08-22 11:30:08 +0300

    Setup clipboard

---
+++ nvim-lua/lua/ettaka/clipboard.lua
@@ -0,0 +1,14 @@
+-- Remember to install a clipboard manager: for example xclip
+-- vim.api.nvim_set_option("clipboard","unnamed")
+
+-- Copy to clipboard
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+yg_')
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>yy', '"+yy')
+
+-- Paste from clipboard
+vim.keymap.set('n', '<leader>p', '"+p')
+vim.keymap.set('n', '<leader>P', '"+P')
+vim.keymap.set('v', '<leader>p', '"+p')
+vim.keymap.set('v', '<leader>P', '"+P')
