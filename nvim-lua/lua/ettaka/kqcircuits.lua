2f5fe73846a06e4d90a41d397379d5d05e054eca
Eelis Takala <eelis@meetiqm.com>
2024-01-03 15:53:59 +0200

    Add mapping for opening element in KQCircuits

--- nvim-lua/lua/ettaka/kqcircuits.lua
+++ nvim-lua/lua/ettaka/kqcircuits.lua
@@ -16,5 +16,15 @@ function KQC_run_current_file()
     vim.api.nvim_feedkeys(cmd, "i", true)
 end
 
+function KQC_open_element()
+    local file = vim.fn.expand('%')
+    local enter_key = vim.api.nvim_replace_termcodes("<CR>", true, false, true)
+    local cmd = "ikl "..file..enter_key.."exit"
+    vim.cmd("sp")
+    vim.cmd("te")
+    vim.api.nvim_feedkeys(cmd, "i", true)
+end
+
+vim.keymap.set("n", "<leader>kl", KQC_open_element)
 vim.keymap.set("n", "<leader><space>", KQC_simulate_current_file)
 vim.keymap.set("n", "<leader><enter>", KQC_run_current_file)
