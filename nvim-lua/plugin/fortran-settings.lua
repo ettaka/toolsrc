b246f33293cb6ff7027f78ae354a2d6ce2ce365c
Eelis Takala <eelis@meetiqm.com>
2024-02-22 16:01:18 +0200

    Fortran conceal change
    It is annoying when scrolling through lines and they flicker.
    There is still one issue: I would like conceal to conserve the width of
    the original text by padding the text with extra whitespace.

--- nvim-lua/plugin/fortran-settings.lua
+++ nvim-lua/plugin/fortran-settings.lua
@@ -1,10 +1,11 @@
 vim.cmd([[
 augroup fortran_settings
   autocmd!
-	autocmd FileType fortran set foldmethod=syntax
+  autocmd FileType fortran set foldmethod=syntax
   autocmd FileType fortran nnoremap <buffer> <localleader>c 0i!<esc>j
   autocmd FileType fortran set tabstop=2 softtabstop=0 expandtab shiftwidth=2 smarttab conceallevel=1
   autocmd FileType fortran syntax keyword FortranConceal call conceal cchar=📞
   autocmd FileType fortran syntax match FortranConceal '%' conceal cchar=→
+  autocmd FileType fortran set concealcursor=niv
 augroup END
 "]])
