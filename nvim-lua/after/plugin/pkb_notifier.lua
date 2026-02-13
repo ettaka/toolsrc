bb05f7cdf3d62b4cb49f73f505f781a4851d9e6f
Eelis Takala <eelis@meetiqm.com>
2026-02-13 08:50:33 +0200

    Add pkb notifier autocommands

--- nvim-lua/after/plugin/pkb_notifier.lua
+++ nvim-lua/after/plugin/pkb_notifier.lua
@@ -1,1 +1,31 @@
-require("pkb_notifier").notify()
+local pkb = require("pkb_notifier")
+
+-- run once at startup
+vim.defer_fn(function()
+  pkb.notify()
+end, 100)
+
+local timer
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*.md",
+  callback = function(args)
+    local path = vim.api.nvim_buf_get_name(args.buf)
+    if path == "" then return end
+
+    local pkb_root = vim.fn.expand("~/pkb")
+
+    if not path:find(pkb_root, 1, true) then
+      return
+    end
+
+    if timer then
+      timer:stop()
+    end
+
+    timer = vim.defer_fn(function()
+      pkb.notify()
+    end, 500)
+  end,
+})
+
