8232dc4eaf29ecb1b032852f53edd14639e41127
Eelis Takala <eelis@meetiqm.com>
2026-02-13 08:28:52 +0200

    Calendar mapping for insert mode

--- nvim-lua/after/plugin/calendar.lua
+++ nvim-lua/after/plugin/calendar.lua
@@ -1,3 +1,8 @@
-vim.keymap.set("n", "<leader>c", function()
+vim.keymap.set({"n"}, "<leader>c", function()
   require("calendar").open()
 end, { desc = "Open calendar" })
+
+vim.keymap.set({"i"}, "<leader>c", function()
+  require("calendar").open()
+  vim.api.nvim_input("<esc>")
+end, { desc = "Open calendar in insert mode" })
