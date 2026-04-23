d1efc9556951d1a0e038ec5a9e2dafcb63068724
Eelis Takala <eelis@meetiqm.com>
2026-04-23 08:08:54 +0300

    Add week number print when opening calendar

--- nvim-lua/after/plugin/calendar.lua
+++ nvim-lua/after/plugin/calendar.lua
@@ -1,8 +1,10 @@
 vim.keymap.set({"n"}, "<leader>c", function()
+  print(os.date("%V"))
   require("calendar").open()
 end, { desc = "Open calendar" })
 
 vim.keymap.set({"i"}, "<leader>c", function()
+  print(os.date("%V"))
   require("calendar").open()
   vim.api.nvim_input("<esc>")
 end, { desc = "Open calendar in insert mode" })
